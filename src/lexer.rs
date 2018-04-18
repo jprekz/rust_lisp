a4b2d9a62246d04628265722edb1c44302657fd8
jprekz <jprekz@users.noreply.github.com>
2018-04-19 08:20:39 +0900

    first commit

---
+++ src/lexer.rs
@@ -0,0 +1,82 @@
+use std::iter::{Iterator, Peekable};
+
+#[derive(Debug, Clone)]
+pub enum Token {
+    LPER,
+    RPER,
+    QUOTE,
+    DOT,
+    BOOL(bool),
+    IDENT(String),
+    NUM(f64),
+}
+
+pub struct Lexer<C: Iterator<Item = char>> {
+    reader: Peekable<C>,
+}
+impl<C: Iterator<Item = char>> Lexer<C> {
+    pub fn new(reader: C) -> Lexer<C> {
+        Lexer {
+            reader: reader.peekable(),
+        }
+    }
+}
+impl<C: Iterator<Item = char>> Iterator for Lexer<C> {
+    type Item = Token;
+    fn next(&mut self) -> Option<Token> {
+        let mut buf = String::new();
+        while let Some(ch) = self.reader.next() {
+            if is_identifier_char(ch) {
+                buf.push(ch);
+            }
+            let peek = self.reader.peek().map(|o| o.clone());
+            match (ch, peek) {
+                ('(', _) => return Some(Token::LPER),
+                (')', _) => return Some(Token::RPER),
+                ('\'', _) => return Some(Token::QUOTE),
+                ('.', None) => return Some(Token::DOT),
+                ('.', Some(peek)) if !is_identifier_char(peek) => return Some(Token::DOT),
+                ('#', Some(_)) => {
+                    match self.reader.next().unwrap() {
+                        't' => {
+                            return Some(Token::BOOL(true));
+                        },
+                        'f' => {
+                            return Some(Token::BOOL(false));
+                        },
+                        _ => panic!(),
+                    }
+                },
+                (_, _) if is_identifier_char(ch) => {
+                    if let Some(peek) = peek {
+                        if is_identifier_char(peek) {
+                            continue;
+                        }
+                    }
+                    if let Ok(num) = buf.parse() {
+                        return Some(Token::NUM(num));
+                    } else {
+                        return Some(Token::IDENT(buf));
+                    }
+                }
+                _ => (),
+            }
+        }
+        None
+    }
+}
+
+fn is_identifier_char(ch: char) -> bool {
+    ch.is_ascii_alphabetic() ||
+    ('-' <= ch && ch <= ':') ||
+    ('<' <= ch && ch <= '@') ||
+    ch == '_' ||
+    ch == '*' ||
+    ch == '+' ||
+    ch == '!' ||
+    ch == '$' ||
+    ch == '%' ||
+    ch == '&' ||
+    ch == '^' ||
+    ch == '~'
+}
---
+++ src/main.rs
@@ -0,0 +1,148 @@
+#![feature(io)]
+
+mod lexer;
+use lexer::*;
+
+use std::cell::RefCell;
+use std::io::{stdin, Read};
+use std::iter::{Iterator, Peekable};
+use std::rc::Rc;
+
+fn main() {
+    let lexer = Lexer::new(stdin().chars().filter_map(|r| r.ok()));
+    /*
+    let lexer = Lexer::new("sss".chars());
+    for token in lexer {
+        println!("{:?}", token);
+    }
+    */
+    let value = eval(&mut lexer.peekable());
+    println!("{:?}", value);
+}
+
+fn eval<T: Iterator<Item = Token>>(token_stream: &mut Peekable<T>) -> Value {
+    match token_stream.next().unwrap() {
+        Token::LPER => {
+            if let Some(Token::RPER) = token_stream.peek() {
+                token_stream.next();
+                return Value::Nil;
+            }
+            if let Some(Token::IDENT(func_ident)) = token_stream.next() {
+                let value = match func_ident.as_ref() {
+                    "quote" => syntax_quote(token_stream),
+                    "lambda" => syntax_lambda(token_stream),
+                    "cons" => syntax_cons(token_stream),
+                    _ => panic!("invalid application"),
+                };
+                if let Some(Token::RPER) = token_stream.next() {
+                    return value;
+                } else {
+                    panic!("syntax error");
+                }
+            } else {
+                panic!("invalid application");
+            }
+        }
+        Token::RPER => panic!("syntax error"),
+        Token::QUOTE => syntax_quote(token_stream),
+        Token::DOT => panic!("syntax error"),
+        Token::BOOL(b) => Value::Bool(b),
+        Token::IDENT(ident) => panic!("unbound variable: {}", ident),
+        Token::NUM(num) => Value::Num(num),
+    }
+}
+
+fn syntax_quote<T: Iterator<Item = Token>>(token_stream: &mut Peekable<T>) -> Value {
+    match token_stream.next().unwrap() {
+        Token::LPER => (),
+        Token::QUOTE => {
+            return Value::Quoted(rr_new(syntax_quote(token_stream)));
+        },
+        Token::BOOL(b) => {
+            return Value::Bool(b);
+        },
+        Token::IDENT(ident) => {
+            return Value::Ident(ident);
+        }
+        Token::NUM(num) => {
+            return Value::Num(num);
+        }
+        _ => panic!(),
+    }
+    if let Some(Token::IDENT(ident)) = token_stream.peek().map(|c| c.clone()) {
+        if ident.eq("quote") {
+            token_stream.next();
+            let value = Value::Quoted(rr_new(syntax_quote(token_stream)));
+            if let Some(Token::RPER) = token_stream.next() {
+                return value;
+            } else {
+                panic!("syntax error");
+            }
+        }
+    }
+    let mut tail = rr_new(Value::Nil);
+    let head = Value::Cons(rr_new(syntax_quote(token_stream)), tail.clone());
+    while let Some(peek) = token_stream.peek().map(|c| c.clone()) {
+        match peek {
+            Token::RPER => {
+                token_stream.next();
+                return head;
+            },
+            Token::DOT => {
+                token_stream.next();
+                let value = syntax_quote(token_stream);
+                tail.replace(value);
+                if let Some(Token::RPER) = token_stream.next() {
+                    return head;
+                } else {
+                    panic!();
+                }
+            },
+            _ => {
+                let value = syntax_quote(token_stream);
+                let next_tail = rr_new(Value::Nil);
+                tail.replace(Value::Cons(rr_new(value), next_tail.clone()));
+                tail = next_tail;
+            },
+        }
+    }
+    panic!();
+}
+
+fn syntax_lambda<T: Iterator<Item = Token>>(token_stream: &mut Peekable<T>) -> Value {
+    unimplemented!()
+}
+
+fn syntax_cons<T: Iterator<Item = Token>>(token_stream: &mut Peekable<T>) -> Value {
+    let car = eval(token_stream);
+    let cdr = eval(token_stream);
+    return Value::Cons(rr_new(car), rr_new(cdr));
+}
+
+#[derive(Clone)]
+enum Value {
+    Cons(Rc<RefCell<Value>>, Rc<RefCell<Value>>),
+    Nil,
+    Quoted(Rc<RefCell<Value>>),
+    Bool(bool),
+    Num(f64),
+    Ident(String),
+    Procedure(Vec<Token>, Vec<Token>),
+}
+impl std::fmt::Debug for Value {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Value::Cons(car, cdr) => write!(f, "({:?} . {:?})", car.borrow(), cdr.borrow()),
+            Value::Quoted(value) => write!(f, "'{:?}", value.borrow()),
+            Value::Nil => write!(f, "()"),
+            Value::Bool(b) => if *b { write!(f, "#t") } else { write!(f, "#f") },
+            Value::Num(num) => write!(f, "{}", num),
+            Value::Ident(ident) => write!(f, "{}", ident),
+            Value::Procedure(_, _) => write!(f, "<Procedure>"),
+        }
+    }
+}
+
+fn rr_new<T>(t: T) -> Rc<RefCell<T>> {
+    Rc::new(RefCell::new(t))
+}
