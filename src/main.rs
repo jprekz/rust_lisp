fbc341397de3b0dca7722acb2d6705f32435824a
jprekz <jprekz@users.noreply.github.com>
2018-05-09 17:47:57 +0900

    file input

--- src/main.rs
+++ src/main.rs
@@ -13,18 +13,30 @@ use lexer::Lexer;
 use parser::parse;
 
 use std::io::{stdin, stdout, Read, Write};
+use std::fs::File;
 
 fn main() {
-    let lexer = Lexer::new(stdin().chars().filter_map(|r| r.ok()));
+    if let Some(arg) = std::env::args().nth(1) {
+        let file = File::open(arg).unwrap();
+        repr(file.chars().filter_map(|r| r.ok()));
+    } else {
+        repr(stdin().chars().filter_map(|r| r.ok()));
+    }
+}
+
+fn repr<T>(input: T)
+where T: Iterator<Item=char> + 'static {
+    let lexer = Lexer::new(input);
     let mut lexer = lexer.peekable();
     let env = Env::new_default();
     loop {
         print!("> ");
         stdout().flush().unwrap();
         println!("{:?}", eval(parse(&mut lexer), env.clone()));
-        println!("");
+        println!("{:?}", lexer.peek());
         if let None = lexer.peek() {
             break;
         }
     }
 }
+
